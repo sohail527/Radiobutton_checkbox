4d5edf8f1204b6cf848465a3fe5169ade557bea3
sohail527 <sohail527@users.noreply.github.com>
2020-10-25 23:30:00 +0500

    Initail Commit

--- app/src/main/java/com/example/assignment/Main2Activity.java
+++ app/src/main/java/com/example/assignment/Main2Activity.java
@@ -3,12 +3,35 @@ package com.example.assignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
+    String a1,a2,a3,a4,a5,a6;
+    TextView t1,t2,t3,t4,t5,t6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        t1=(TextView)findViewById(R.id.t1) ;
+        t2=(TextView)findViewById(R.id.t2) ;
+        t3=(TextView)findViewById(R.id.t3) ;
+        t4=(TextView)findViewById(R.id.t4) ;
+        t5=(TextView)findViewById(R.id.t5) ;
+        t6=(TextView)findViewById(R.id.t6) ;
+
+        a1=getIntent().getExtras().getString("a");
+        a2=getIntent().getExtras().getString("b");
+        a3=getIntent().getExtras().getString("c");
+        a4=getIntent().getExtras().getString("d");
+        a5=getIntent().getExtras().getString("e");
+        a6=getIntent().getExtras().getString("f");
+
+        t1.setText(a1);
+        t2.setText(a2);
+        t3.setText("Check1 "+a3);
+        t4.setText("Check2 "+a4);
+        t5.setText("Check3 "+a5);
+        t6.setText("Check4 "+a6);
     }
 }
--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -6,19 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public Button button;
-    public RadioGroup rdgroup;
-    public RadioButton rdbutton;
-
+    public RadioGroup rdgroup1,rdgroup2;
+    public RadioButton rdbutton1,rdbutton2;
+    public CheckBox chbox1,chbox2,chbox3,chbox4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rdgroup1=(RadioGroup)findViewById(R.id.radio1);
+        rdgroup2=(RadioGroup)findViewById(R.id.radio2);
+
+        chbox1=(CheckBox) findViewById(R.id.ch1);
+        chbox2=(CheckBox) findViewById(R.id.ch2);
+        chbox3=(CheckBox) findViewById(R.id.ch3);
+        chbox4=(CheckBox) findViewById(R.id.ch4);
 
         button=(Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -32,14 +41,41 @@ public class MainActivity extends AppCompatActivity {
     }
     public void openActivity2()
     {
+        int i,j;
+        boolean b1,b2,b3,b4;
+        String s1,s2,s3,s4,s5,s6;
+        i=rdgroup1.getCheckedRadioButtonId();
+        j=rdgroup2.getCheckedRadioButtonId();
+        rdbutton1=(RadioButton)findViewById(i);
+        rdbutton2=(RadioButton)findViewById(j);
+        s1=rdbutton1.getText().toString();
+        s2=rdbutton2.getText().toString();
+        b1=chbox1.isChecked();
+        b2=chbox2.isChecked();
+        b3=chbox3.isChecked();
+        b4=chbox4.isChecked();
+        s3=checker(b1);
+        s4=checker(b2);
+        s5=checker(b3);
+        s6=checker(b4);
         Intent intent = new Intent(this,Main2Activity.class);
+        intent.putExtra("a",s1);
+        intent.putExtra("b",s2);
+        intent.putExtra("c",s3);
+        intent.putExtra("d",s4);
+        intent.putExtra("e",s5);
+        intent.putExtra("f",s6);
         startActivity(intent);
     }
-    public void check()
+
+    public String checker(boolean b)
     {
-        int radioid = rdgroup.getCheckedRadioButtonId();
-        rdbutton=findViewById(radioid);
-        Toast.makeText(c)
+        if(b==false)
+        {return "not Checked";}
+        else{
+            return  "checked";
+        }
     }
+
 
 }
